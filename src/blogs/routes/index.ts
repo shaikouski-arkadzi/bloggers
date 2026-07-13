0f3fa40ba99a163703fc13da33737a691a275e46
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-07-13 21:49:00 +0300

    Use id valiation in some routes

--- src/blogs/routes/index.ts
+++ src/blogs/routes/index.ts
@@ -7,13 +7,14 @@ import {
   updateBlog,
 } from "../controllers";
 import { BLOGS_ROUTES } from "../constants";
+import { idValidation } from "../../common/validation";
 
 const router = Router();
 
 router.post(BLOGS_ROUTES.ROOT, createBlog);
 router.get(BLOGS_ROUTES.ROOT, getBlogs);
-router.get(BLOGS_ROUTES.BY_ID, getBlog);
-router.put(BLOGS_ROUTES.BY_ID, updateBlog);
-router.delete(BLOGS_ROUTES.BY_ID, deleteBlog);
+router.get(BLOGS_ROUTES.BY_ID, idValidation, getBlog);
+router.put(BLOGS_ROUTES.BY_ID, idValidation, updateBlog);
+router.delete(BLOGS_ROUTES.BY_ID, idValidation, deleteBlog);
 
 export default router;
--- src/posts/routes/index.ts
+++ src/posts/routes/index.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
-import { createPost, deletePost, getPost, getPosts, updatePost } from "../controllers";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controllers";
 import { POSTS_ROUTES } from "../constants";
+import { idValidation } from "../../common/validation";
 
 const router = Router();
 
 router.post(POSTS_ROUTES.ROOT, createPost);
 router.get(POSTS_ROUTES.ROOT, getPosts);
-router.get(POSTS_ROUTES.BY_ID, getPost);
-router.put(POSTS_ROUTES.BY_ID, updatePost);
-router.delete(POSTS_ROUTES.BY_ID, deletePost);
+router.get(POSTS_ROUTES.BY_ID, idValidation, getPost);
+router.put(POSTS_ROUTES.BY_ID, idValidation, updatePost);
+router.delete(POSTS_ROUTES.BY_ID, idValidation, deletePost);
 
 export default router;
