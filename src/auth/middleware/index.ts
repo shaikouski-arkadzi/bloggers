3e2926bc309d2848ce2ffe311264e59e46be12a7
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-07-16 08:34:23 +0300

    Add superAdminGuardMiddleware

---
+++ src/auth/middleware/index.ts
@@ -0,0 +1,1 @@
+export { superAdminGuardMiddleware } from "./super-admin.guard.middleware";
---
+++ src/auth/middleware/super-admin.guard.middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+
+export const superAdminGuardMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  // Ожидаем строке в authorization заголовке вида 'Basic xxxx'
+  const auth = req.headers["authorization"] as string;
+
+  if (!auth) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const [authType, token] = auth.split(" ");
+
+  if (authType !== "Basic") {
+    res.sendStatus(401);
+    return;
+  }
+
+  const credentials = Buffer.from(token, "base64").toString("utf-8");
+  const [username, password] = credentials.split(":");
+
+  next();
+};
