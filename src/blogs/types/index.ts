2f04761238ec00236fc6ced23f97092b31721c23
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-07-24 08:52:00 +0300

    Refactor: divide db model and output model

--- src/blogs/types/index.ts
+++ src/blogs/types/index.ts
@@ -1,12 +1,15 @@
+import { ObjectId } from "mongodb";
+
 export interface BlogInputDto {
   name: string;
   description: string;
   websiteUrl: string;
 }
 
-export interface Blog {
+export interface BlogDb extends BlogInputDto {
+  _id: ObjectId;
+}
+
+export interface Blog extends BlogInputDto {
   id: string;
-  name: string;
-  description: string;
-  websiteUrl: string;
 }
