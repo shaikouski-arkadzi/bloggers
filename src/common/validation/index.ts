27c52b0ae3583819e62c86ee626c6bc08504ec0e
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-08-10 22:30:10 +0300

    Add pagination and sorting validation common export

--- src/common/validation/index.ts
+++ src/common/validation/index.ts
@@ -1,2 +1,4 @@
 export { idValidation } from "./id.validation.middleware";
 export { resultValidationMiddleware } from "./result.validation.middleware";
+export { paginationValidation } from "./pagination.validation.middleware";
+export { sortingValidation } from "./sorting.validation.middleware";
--- src/common/validation/pagination.validation.middleware.ts
+++ src/common/validation/pagination.validation.middleware.ts
@@ -7,3 +7,5 @@ export const pageNumberValidation = query("pageNumber")
 export const pageSizeValidation = query("pageSize")
   .isInt({ min: 1 })
   .withMessage("Количество элементов на странице должно быть больше 0");
+
+export const paginationValidation = [pageNumberValidation, pageSizeValidation];
