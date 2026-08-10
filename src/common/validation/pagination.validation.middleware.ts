05d8998aae5d29259e9ac4a143c4a9fc778d60b4
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-08-10 22:40:54 +0300

    Add default values in pagination and sorting validations

--- src/common/validation/pagination.validation.middleware.ts
+++ src/common/validation/pagination.validation.middleware.ts
@@ -1,10 +1,13 @@
 import { query } from "express-validator";
+import { PAGE_DAFAULT, PAGE_SIZE_DAFAULT } from "../constants";
 
 export const pageNumberValidation = query("pageNumber")
+  .default(PAGE_DAFAULT)
   .isInt({ min: 1 })
   .withMessage("Номер страницы должен быть больше 0");
 
 export const pageSizeValidation = query("pageSize")
+  .default(PAGE_SIZE_DAFAULT)
   .isInt({ min: 1 })
   .withMessage("Количество элементов на странице должно быть больше 0");
 
--- src/common/validation/sorting.validation.middleware.ts
+++ src/common/validation/sorting.validation.middleware.ts
@@ -2,13 +2,21 @@ import { query } from "express-validator";
 import { SortDirections } from "../types";
 import { Blog } from "../../blogs/types";
 import { Post } from "../../posts/types";
+import { SORT_DIRECTION_DAFAULT, SORT_FIELD_DAFAULT } from "../constants";
 
 export const sortFieldValidation = <T extends Blog | Post>(
   fields: (keyof T)[],
-) => query("sortBy").isIn(fields).withMessage("Проверьте значение в sortBy");
+) =>
+  query("sortBy")
+    .default(SORT_FIELD_DAFAULT)
+    .isIn(fields)
+    .withMessage("Проверьте значение в sortBy");
 
 export const sortDirectionValidation = query("sortDirection")
+  .default(SORT_DIRECTION_DAFAULT)
   .isIn(Object.values(SortDirections))
   .withMessage("Проверьте значение в sortDirection");
 
-export const sortingValidation = [sortFieldValidation, sortDirectionValidation];
+export const sortingValidation = <T extends Blog | Post>(
+  fields: (keyof T)[],
+) => [sortFieldValidation(fields), sortDirectionValidation];
