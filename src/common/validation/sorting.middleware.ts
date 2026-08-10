2b5419fb0247d19b1ac7e1d5ba1680acced2f602
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-08-10 22:23:12 +0300

    Add sortField validation

--- src/common/validation/sorting.middleware.ts
+++ src/common/validation/sorting.middleware.ts
@@ -1,6 +1,14 @@
 import { query } from "express-validator";
 import { SortDirections } from "../types";
+import { Blog } from "../../blogs/types";
+import { Post } from "../../posts/types";
+
+export const sortFieldValidation = <T extends Blog | Post>(
+  fields: (keyof T)[],
+) => query("sortBy").isIn(fields).withMessage("Проверьте значение в sortBy");
 
 export const sortDirectionValidation = query("sortDirection")
   .isIn(Object.values(SortDirections))
   .withMessage("Проверьте значение в sortDirection");
+
+export const sortingValidation = [sortFieldValidation, sortDirectionValidation];
